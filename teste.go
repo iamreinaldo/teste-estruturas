7b8fe7e5ee88e8d6d32c9589dfc2efc33ce857fb
Reinaldo <iamreinaldoneto@icloud.com>
2024-03-18 16:32:04 -0300

    Finalizando versão 0.1

--- teste.go
+++ teste.go
@@ -3,11 +3,15 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io"
 	"os"
 	"os/exec"
 	"strings"
 )
+
+type Endereco struct {
+	IP   string
+	Nome string
+}
 
 func verApp() {
 	versao := 0.1
@@ -22,27 +26,35 @@ func lerComando() int {
 
 func mainMenu() {
 	fmt.Println("1 - Iniciar monitoramento")
-	fmt.Println("2 - Exibir logs")
-	fmt.Println("0 - Sair do programa")
+	// fmt.Println("2 - Exibir logs")
+	fmt.Printf("0 - Sair do programa\n\n")
+
 }
 
 func monitoraMenu() {
 	fmt.Println("1 - Genipapo/ Malhadinha/ Balança")
 	fmt.Println("2 - OLTs")
-	fmt.Println("3 - UNM")
-	fmt.Println("Qualquer tecla para voltar ao menu iniciar")
+	fmt.Println("3 - Lages do Batata")
+	fmt.Println("4 - Paraíso")
+	fmt.Println("5 - Palmeirinha")
+	fmt.Printf("Qualquer tecla para voltar ao menu iniciar\n\n")
 }
 
 func iniciarMonitoramento(nome string) {
 	fmt.Println("Pingando...")
 	enderecos := lerEnderecosArquivo(nome)
+	if enderecos == nil {
+		fmt.Println("Não há endereços para monitorar")
+		return
+	}
+
 	for _, enderecos := range enderecos {
 		ping(enderecos)
 	}
 }
 
-func lerEnderecosArquivo(nome string) []string {
-	var ips []string
+func lerEnderecosArquivo(nome string) []Endereco {
+	var ips []Endereco
 	arquivo, err := os.Open(nome)
 
 	if err != nil {
@@ -50,31 +62,38 @@ func lerEnderecosArquivo(nome string) []string {
 	}
 
 	defer arquivo.Close()
-	leitor := bufio.NewReader(arquivo)
-	for {
-		linha, err := leitor.ReadString('\n')
-		linha = strings.TrimSpace(linha)
+
+	scanner := bufio.NewScanner(arquivo)
+	for scanner.Scan() {
+		linha := strings.TrimSpace(scanner.Text())
 		if linha != "" {
-			ips = append(ips, linha)
-		}
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			fmt.Println("Erro ao ler a linha: ", err)
-			break
+			partes := strings.Split(linha, ";")
+			if len(partes) == 2 {
+				ips = append(ips, Endereco{
+					IP:   partes[0],
+					Nome: partes[1],
+				})
+			}
 		}
 	}
-	fmt.Println("Pingando para os seguintes endereços: ", ips)
+
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("Erro ao ler o arquivo: %v\n", err)
+		return nil
+	}
+
+	for _, endereco := range ips {
+		fmt.Printf("Pingando para %s (%s)\n", endereco.Nome, endereco.IP)
+	}
 	return ips
 }
 
-func ping(enderecos string) {
-	err := exec.Command("ping", "-c", "4", enderecos).Run()
+func ping(enderecos Endereco) {
+	err := exec.Command("ping", "-c", "4", enderecos.IP).Run()
 	if err == nil {
-		fmt.Printf("Ping para %s bem-sucedido. \n\n", enderecos)
+		fmt.Printf("\nPing para %s bem-sucedido. \n", enderecos.Nome)
 	} else {
-		fmt.Printf("Falha ao executar ping para %s. \n\n", enderecos)
+		fmt.Printf("\nFalha ao executar ping para %s. \n", enderecos.Nome)
 	}
 }
 
@@ -92,7 +111,11 @@ func main() {
 		case 2:
 			iniciarMonitoramento("olt.txt")
 		case 3:
-			iniciarMonitoramento("unm.txt")
+			iniciarMonitoramento("paraiso.txt")
+		case 4:
+			iniciarMonitoramento("lages.txt")
+		case 5:
+			iniciarMonitoramento("palmeirinha.txt")
 		default:
 			os.Exit(-1)
 		}
